b8496669aadda676c1754455d2c38801cb11f6e4
Yann Rochard <yann.rochard@gmail.com>
2011-05-26 17:29:05 +0200

    Création architecture Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torche::Application.config.secret_token = '2fbe7228b343206cbfe05200abf1112602a67559aa29e3d6f7f4841add32d37918dd8df39507ff6f6f8e94ed021751259ef6ebd9d94cd47496a9b9561440fd27'
